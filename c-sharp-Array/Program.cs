e31fb259bf8e2a430ca08d0ea962d61da47740c5
omranali2111 <aljabri1234@hotmail.com>
2023-06-11 11:08:29 +0400

    task1

--- c-sharp-Array/Program.cs
+++ c-sharp-Array/Program.cs
@@ -1,4 +1,8 @@
 ﻿using static System.Console;
+
+using System;
+using System.Xml.Linq;
+
 internal class Program
 {
     private static void Main(string[] args)
@@ -12,5 +16,28 @@ internal class Program
         }
 
         WriteLine(sum);
+
+        //task1 :Write a program that accepts an array of integers and calculates the average 
+        //of all the elements
+        int[] num = { 1, 5, 7, 9, 16 };
+       // double avrg = 0.0;
+        int sum1 = 0;
+        for (int i = 0; i < num.Length; i++)
+        {
+            sum1 = sum1 + num[i];
+           
+            
+        }
+
+        double avrg = sum1 / num.Length;
+        WriteLine(avrg);
+
+
+        //task2:Write a program that accepts an array of integers and finds the maximum 
+        //and minimum elements in the array.
+
     }
+    
+
+
 }
