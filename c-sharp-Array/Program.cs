73b41e33a39a706090e223294a09a2664a6fd96d
omranali2111 <aljabri1234@hotmail.com>
2023-06-11 10:21:34 +0400

    practice of Array

---
+++ c-sharp-Array/Program.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] a = { 1, 5, 7, 9, 10 };
+        int sum = 0;
+        for(int i=0; i<a.Length; i++)
+        {
+            sum = sum + a[i];
+
+        }
+
+        WriteLine(sum);
+    }
+}
